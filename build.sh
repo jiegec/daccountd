01d4ca2d4e5c87119454e48dd0a3e23165c1514f
Jiajie Chen <c@jia.je>
2021-02-14 21:01:55 +0800

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+go build -o daccountd
+export CGO_ENABLED=0
+GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o daccountd-windows-amd64
+GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -o daccountd-darwin-amd64
+GOOS=darwin GOARCH=arm64 go build -ldflags="-s -w" -o daccountd-darwin-arm64
+GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o daccountd-linux-amd64
+GOOS=linux GOARCH=arm64 go build -ldflags="-s -w" -o daccountd-linux-arm64
+GOOS=linux GOARCH=ppc64le go build -ldflags="-s -w" -o daccountd-linux-ppc64le
+GOOS=linux GOARCH=mipsle go build -ldflags="-s -w" -o daccountd-linux-mipsle
